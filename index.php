1ca3a5f2291541407bc9a0546a81c7587e82e3b5
jvssantiago <ssilva.joaovictor@gmail.com>
2026-06-11 08:44:50 -0300

    Adicionado o menu do projeto

--- index.php
+++ index.php
@@ -1,30 +1,9 @@
 <?php
-//OBSERVAÇÃO: 
-// include: Emite um aviso (Warning) e continua a execução do script.
-// require: Emite um erro fatal (Fatal Error) e interrompe a execução do script
-
-//o uso de once Verifica se o arquivo já foi incluído ou carregado. Se sim, não o inclui ou carrega novamente
-
-// Inclui o topo da página (Tags HTML, Head)
 require_once 'modulos/topo.php';
 
-// Inclui o menu de navegação
-require_once 'modulos/menu.php';
+include_once 'modulos/menu.php';
 ?>
 
-<main class="container">
-    <div class="row">
-        <?php 
-        // Inclui a seção de conteúdo principal
-        require_once 'modulos/conteudo.php'; 
-        
-        // Inclui a barra lateral
-        require_once 'modulos/lateral.php'; 
-        ?>
-    </div>
-</main>
-
 <?php
-// Inclui o rodapé (Fechamento do HTML)
 require_once 'modulos/rodape.php';
 ?>
--- modulos/menu.php
+++ modulos/menu.php
@@ -1,11 +1,12 @@
 <nav class="nav container">
     <div class="nav-left">
-        <a class="brand" href="#">Logotipo</a>
+        <a class="brand" href="#">LandingPage</a>
     </div>
     <div class="nav-right">
         <a href="#home" class="tabs">Início</a>
         <a href="#recursos" class="tabs">Recursos</a>
         <a href="#contato" class="tabs">Contato</a>
+        <a href="#localizacao" class= "tabs">Localização</a>
     </div>
 </nav>
 <hr>
